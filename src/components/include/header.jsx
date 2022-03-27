f87fce561b62c3d0f56077756b1d994f144a9617
btamega <bougarytamega77@gmail.com>
2022-03-27 12:50:31 +0000

    commit du dimanche 27/03/2022

--- src/components/include/header.jsx
+++ src/components/include/header.jsx
@@ -14,7 +14,7 @@ const header = () => {
             <div className="topbar">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-4 col-md-12 col-12">
+                        <div className="col-lg-6 col-md-6 col-6">
                             <div className="top-left">
                                 <ul className="list-main">
                                     <li><a href="tel:+33 (605) 703-920"><i className="ti-headphone-alt"></i> +33 (605) 703-920</a></li>
@@ -22,7 +22,7 @@ const header = () => {
                                 </ul>
                             </div>
                         </div>
-                        <div className="col-lg-8 col-md-12 col-12">
+                        <div className="col-lg-6 col-md-6 col-6">
                             <div className="right-content">
                                 <ul className="list-main">
                                     <li><a target={'_blank'} href="https://web.facebook.com/Khassodesign-112125716933890"><i className="ti-facebook"></i> KhassoDesign</a></li>
@@ -118,79 +118,9 @@ const header = () => {
                 <div className="container">
                     <div className="cat-nav-head">
                         <div className="row">
-                            <div className="col-lg-3">
-                                <div className="all-category">
-                                    <h3 onClick={openNav} className="cat-heading">
-                                    <a  className='closebtn' onClick={closeNav}>&times;</a>
-                                      <i className="fa fa-bars" aria-hidden="true"></i>CATEGORIES</h3>
-                                    <ul id='category' className="main-category">
-                                        <li><a href="#">New Arrivals <i className="fa fa-angle-right" aria-hidden="true"></i></a>
-                                            <ul className="sub-category">
-                                                <li><a href="#">accessories</a></li>
-                                                <li><a href="#">best selling</a></li>
-                                                <li><a href="#">top 100 offer</a></li>
-                                                <li><a href="#">sunglass</a></li>
-                                                <li><a href="#">watch</a></li>
-                                                <li><a href="#">man’s product</a></li>
-                                                <li><a href="#">ladies</a></li>
-                                                <li><a href="#">westrn dress</a></li>
-                                                <li><a href="#">denim </a></li>
-                                            </ul>
-                                        </li>
-                                        <li className="main-mega"><a href="#">best selling <i className="fa fa-angle-right" aria-hidden="true"></i></a>
-                                            <ul className="mega-menu">
-                                                <li className="single-menu">
-                                                    <a href="#" className="title-link">Shop Kid's</a>
-                                                    <div className="image">
-                                                        <img src="https://via.placeholder.com/225x155" alt="#" />
-                                                    </div>
-                                                    <div className="inner-link">
-                                                        <a href="#">Kids Toys</a>
-                                                        <a href="#">Kids Travel Car</a>
-                                                        <a href="#">Kids Color Shape</a>
-                                                        <a href="#">Kids Tent</a>
-                                                    </div>
-                                                </li>
-                                                <li className="single-menu">
-                                                    <a href="#" className="title-link">Shop Men's</a>
-                                                    <div className="image">
-                                                        <img src="https://via.placeholder.com/225x155" alt="#" />
-                                                    </div>
-                                                    <div className="inner-link">
-                                                        <a href="#">Watch</a>
-                                                        <a href="#">T-shirt</a>
-                                                        <a href="#">Hoodies</a>
-                                                        <a href="#">Formal Pant</a>
-                                                    </div>
-                                                </li>
-                                                <li className="single-menu">
-                                                    <a href="#" className="title-link">Shop Women's</a>
-                                                    <div className="image">
-                                                        <img src="https://via.placeholder.com/225x155" alt="#" />
-                                                    </div>
-                                                    <div className="inner-link">
-                                                        <a href="#">Ladies Shirt</a>
-                                                        <a href="#">Ladies Frog</a>
-                                                        <a href="#">Ladies Sun Glass</a>
-                                                        <a href="#">Ladies Watch</a>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">accessories</a></li>
-                                        <li><a href="#">top 100 offer</a></li>
-                                        <li><a href="#">sunglass</a></li>
-                                        <li><a href="#">watch</a></li>
-                                        <li><a href="#">man’s product</a></li>
-                                        <li><a href="#">ladies</a></li>
-                                        <li><a href="#">westrn dress</a></li>
-                                        <li><a href="#">denim </a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="col-lg-9 col-12">
+                            <div className="col-2"></div>
+                            <div className="col-8">
                                 <div className="menu-area">
-                                    
                                     <nav className="navbar navbar-expand-lg">
                                         <div className="navbar-collapse">	
                                             <div className="nav-inner">	
@@ -212,6 +142,8 @@ const header = () => {
                                                             </ul>
                                                         </li>
                                                         <li><a href="/contact">Contact Us</a></li>
+                                                        <li><a href="/contact">About</a></li>
+
                                                     </ul>
                                             </div>
                                         </div>
@@ -219,6 +151,7 @@ const header = () => {
                                         
                                 </div>
                             </div>
+                            <div className="col-2"></div>
                         </div>
                     </div>
                 </div>
--- src/components/views/contact.jsx
+++ src/components/views/contact.jsx
@@ -10,44 +10,44 @@ const contact = () => {
 						<div className="col-lg-8 col-12">
 							<div className="form-main">
 								<div className="title">
-									<h4>Get in touch</h4>
-									<h3>Write us a message</h3>
+									{/* <h4>Get in touch</h4> */}
+									<h3>Ecrivez nous !</h3>
 								</div>
-								<form className="form" method="post" action="mail/mail.php">
+								<form className="form" action='#' >
 									<div className="row">
 										<div className="col-lg-6 col-12">
 											<div className="form-group">
-												<label>Your Name<span>*</span></label>
+												<label>Nom<span>*</span></label>
 												<input name="name" type="text" placeholder=""/>
 											</div>
 										</div>
 										<div className="col-lg-6 col-12">
 											<div className="form-group">
-												<label>Your Subjects<span>*</span></label>
+												<label>Objet<span>*</span></label>
 												<input name="subject" type="text" placeholder=""/>
 											</div>
 										</div>
 										<div className="col-lg-6 col-12">
 											<div className="form-group">
-												<label>Your Email<span>*</span></label>
+												<label>Email<span>*</span></label>
 												<input name="email" type="email" placeholder=""/>
 											</div>	
 										</div>
 										<div className="col-lg-6 col-12">
 											<div className="form-group">
-												<label>Your Phone<span>*</span></label>
+												<label>Téléphone<span>*</span></label>
 												<input name="company_name" type="text" placeholder=""/>
 											</div>	
 										</div>
 										<div className="col-12">
 											<div className="form-group message">
-												<label>your message<span>*</span></label>
+												<label>Message<span>*</span></label>
 												<textarea name="message" placeholder=""></textarea>
 											</div>
 										</div>
 										<div className="col-12">
 											<div className="form-group button">
-												<button type="submit" className="btn ">Send Message</button>
+												<button type="submit" className="btn ">Envoyer</button>
 											</div>
 										</div>
 									</div>
@@ -60,23 +60,24 @@ const contact = () => {
 									<i className="fa fa-phone"></i>
 									<h4 className="title">Call us Now:</h4>
 									<ul>
-										<li>+123 456-789-1120</li>
-										<li>+522 672-452-1120</li>
+										<li><a href="tel:+33 (605) 703-920">+33 (605) 703-920</a></li>
+										<li><a href="tel:+33 (605) 703-920">+212 620-783-837</a></li>
 									</ul>
 								</div>
 								<div className="single-info">
 									<i className="fa fa-envelope-open"></i>
 									<h4 className="title">Email:</h4>
 									<ul>
-										<li><a href="mailto:info@yourwebsite.com">info@yourwebsite.com</a></li>
-										<li><a href="mailto:info@yourwebsite.com">support@yourwebsite.com</a></li>
+										<li><a href="mailto:info@yourwebsite.com">karecherif2000@gmail.com</a></li>
+										<li><a href="mailto:info@yourwebsite.com">bougarytamega77@gmail.com</a></li>
 									</ul>
 								</div>
 								<div className="single-info">
 									<i className="fa fa-location-arrow"></i>
 									<h4 className="title">Our Address:</h4>
 									<ul>
-										<li>KA-62/1, Travel Agency, 45 Grand Central Terminal, New York.</li>
+										<li>France</li>
+										<li>Maroc</li>
 									</ul>
 								</div>
 							</div>
